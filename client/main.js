046a0a6a03c81a3ae4217a38415a525b96c00a96
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2026-01-09 19:50:43 +0200

    nav buttons added

--- client/main.js
+++ client/main.js
@@ -2,23 +2,44 @@ import { Header } from "@components/header.js";
 import { Footer } from "@components/footer.js";
 import { HomePage } from "@pages/home-page.js";
 import { SigninPage } from "@pages/signin-page.js";
-
-const header = new Header();
-const footer = new Footer()
-const homePage = new HomePage()
-const signinPage = new SigninPage()
+import { Router } from "./src/router.js";
 
 const rootBox = document.getElementById("root");
 
-function router() {
+const header = new Header();
+const footer = new Footer();
+const homePage = new HomePage();
+const signinPage = new SigninPage();
 
+const routes = [
+  { path: "/", template: homePage.render() },
+  { path: "/signin", template: signinPage.render() },
+];
+
+const router = new Router(routes);
+console.log(router);
+
+function render() {
+  const main = document.createElement("main");
+  let page;
   rootBox.appendChild(header.render());
-  //rootBox.appendChild(homePage.render())
-  rootBox.appendChild(signinPage.render())
-  rootBox.appendChild(footer.render())
-
+  //навигационные кнопки в header
+  Array.from(document.querySelectorAll(".header__actions-btn")).forEach(
+    (actionBtn) => {
+      actionBtn.addEventListener("click", (e) => {
+        if (main.children.length > 0) {
+          Array.from(main.children).map((el) => el.remove());
+        }
+        page = router.loadRoute(actionBtn.dataset.path);
+        main.appendChild(page);
+      });
+    }
+  );
+  main.appendChild(router.loadRoute(""));
+  rootBox.appendChild(main);
+  rootBox.appendChild(footer.render());
 }
 
 window.addEventListener("load", () => {
-  router();
+  render();
 });
