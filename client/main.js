e4107904e5d59f51bd540bf20e35cb389221180b
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2026-01-10 18:19:16 +0200

    update

--- client/main.js
+++ client/main.js
@@ -2,44 +2,45 @@ import { Header } from "@components/header.js";
 import { Footer } from "@components/footer.js";
 import { HomePage } from "@pages/home-page.js";
 import { SigninPage } from "@pages/signin-page.js";
-import { Router } from "./src/router.js";
 
 const rootBox = document.getElementById("root");
 
-const header = new Header();
-const footer = new Footer();
-const homePage = new HomePage();
-const signinPage = new SigninPage();
+let header = new Header();
+let footer = new Footer();
+let homePage = new HomePage();
+let signinPage = new SigninPage();
 
-const routes = [
-  { path: "/", template: homePage.render() },
-  { path: "/signin", template: signinPage.render() },
-];
-
-const router = new Router(routes);
-console.log(router);
+const routes = {
+  "#/": homePage.render(),
+  "#/signin": signinPage.render(),
+};
+const main = document.createElement("main");
 
 function render() {
-  const main = document.createElement("main");
-  let page;
-  rootBox.appendChild(header.render());
-  //навигационные кнопки в header
-  Array.from(document.querySelectorAll(".header__actions-btn")).forEach(
-    (actionBtn) => {
-      actionBtn.addEventListener("click", (e) => {
-        if (main.children.length > 0) {
-          Array.from(main.children).map((el) => el.remove());
-        }
-        page = router.loadRoute(actionBtn.dataset.path);
-        main.appendChild(page);
-      });
-    }
-  );
-  main.appendChild(router.loadRoute(""));
+  console.log("location", window.location);
+  const hash = window.location.hash || "/#";
+  let page = routes[hash] ? routes[hash] : null;
+  console.log(page);
+
+  const headerBox = header.render();
+  rootBox.appendChild(headerBox);
+  main.appendChild(page);
   rootBox.appendChild(main);
-  rootBox.appendChild(footer.render());
+  const footerBox = footer.render();
+  rootBox.appendChild(footerBox);
 }
 
-window.addEventListener("load", () => {
-  render();
+window.addEventListener("load", render);
+
+window.addEventListener("hashchange", (e) => {
+  if (e.oldURL !== e.newURL) {
+    const newUrl = new URL(e.newURL);
+    const { hash: newHash } = newUrl;
+
+    if (main.children.length > 0) {
+      Array.from(main.children).map((child) => child.remove());
+      let page = routes[newHash];
+      main.appendChild(page);
+    }
+  }
 });
